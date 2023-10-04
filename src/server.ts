99896ee9625e9e32cf9d93ab13047fbc70c61b6f
Laiz <laizsc.89@gmail.com>
2023-10-04 18:11:45 -0300

    Adiciona suporte ao CORS

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { router } from "./routes/indexRouter";
-
+import cors from "cors";
 
 const serverPort = 3000;
 
 const server = express();
 
 server.use(express.json());
-
+server.use(cors());
 server.use(router);
 
 server.listen(serverPort, () => console.log(`Server running on port ${serverPort}`));
